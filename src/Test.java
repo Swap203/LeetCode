b248c541a930370e85139d8a46236dbfbf390fed
Swap203 <swapnil.pawale20@gmail.com>
2022-08-05 22:28:09 +0530

    Added test class

---
+++ src/Test.java
@@ -0,0 +1,26 @@
+
+public class Test {
+
+	private String testName;
+	private Integer testage;
+	
+	public Test(String testName, Integer testage) {
+		super();
+		this.testName = testName;
+		this.testage = testage;
+	}
+	
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+	public Integer getTestage() {
+		return testage;
+	}
+	public void setTestage(Integer testage) {
+		this.testage = testage;
+	}
+	
+}
